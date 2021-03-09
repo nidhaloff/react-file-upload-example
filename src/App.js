4d79ecb8d6cc01f752782694a35092b7a887f6fe
nidhal baccouri <nidhalbacc@gmail.com>
2021-03-09 21:55:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+
 
 function App() {
+  const [content, setContent] = useState("")
+
+  const showFile = async (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = async (e) => { 
+      const text = (e.target.result)
+      console.log(text)
+      setContent(text)
+    };
+    reader.readAsText(e.target.files[0])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+     Hello world
+     <div></div>
+     <input type="file" onChange={(e) => showFile(e)} />
+     <br></br>
+     <button onClick={() => console.log("button clicked")}>Submit</button>
+
+     <div>{content}</div>
     </div>
   );
 }
